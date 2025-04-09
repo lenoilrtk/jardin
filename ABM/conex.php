f78b6310c8e11be40a640bcda6eb10ae22b90b34
lenoilrtk <lionelfrate340@gmail.com>
2025-04-09 13:14:43 -0300

    añadido archivo conex en carpeta ABM

---
+++ ABM/conex.php
@@ -0,0 +1,23 @@
+<?php
+
+$servername="localhost";
+
+$username = "root";
+
+$password = "";
+
+$dbname="chatbot_sencillo";
+
+// Crear la conexión
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+
+if ($conn->connect_error) {
+
+die("Conexión fallida: ".$conn->connect_error);
+
+}
+
+?>
