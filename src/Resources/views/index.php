0830c534c7a3ccd3f81f33ca88500f8c158868b0
RUYKx <aaroncascino@gmail.com>
2025-08-06 21:20:47 -0300

    composer y vite, hay que hacer composer install y npm install

--- src/Resources/views/index.php
+++ src/Resources/views/index.php
@@ -29,7 +29,7 @@ $generos = [
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-
+    <script type="module" src="http://localhost:5173/Resources/Js/main.js"></script>
     <style>
         * {
             margin: 0;
